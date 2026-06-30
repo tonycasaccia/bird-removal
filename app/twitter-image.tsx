88b7de2d9986257aa3e905a236c7c7bb47cbb955
Tony Casaccia <tonycasaccia@Mac.lan>
2026-06-29 20:06:23 -0700

    Fix twitter image metadata route

--- app/twitter-image.tsx
+++ app/twitter-image.tsx
@@ -1,1 +1,33 @@
-export { runtime, alt, size, contentType, default } from './opengraph-image'
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+
+export const alt = "Wildlife Removal Services";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+export const contentType = "image/png";
+
+export default function Image() {
+  return new ImageResponse(
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: 64,
+        fontWeight: 700,
+        backgroundColor: "#0f172a",
+        color: "white",
+      }}
+    >
+      Wildlife Removal Services
+    </div>,
+    {
+      ...size,
+    },
+  );
+}
