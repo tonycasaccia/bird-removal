a65597cf57c8a768c6d12e2c3ce5f51225738aad
Tony Casaccia <tonycasaccia@Mac.lan>
2026-06-29 18:06:09 -0700

    Remove Next.js build artifacts

--- .next/dev/static/chunks/app_layout_tsx_0r.2mg_._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules__pnpm_01nigu.._.js","static/chunks/_0h-kshb._.js","static/chunks/[root-of-the-server]__06.-pfn._.css"],
-    source: "dynamic"
-});
